726b6365548849fd84e006afd3b4ea9ee2cfcbb4
feynman <2901954062@qq.com>
2022-04-16 20:45:56 +0800

    ts-learn add gulp进行ts打包添加

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import './style.css'
 
-let y: number
-y = null!
-
-const app = document.querySelector < HTMLDivElement > ('#app') !
-
+/* const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
-console.log('hello vite')
+console.log('hello vite') */
+let el: any
 
+?: string | Element  = '123';
 
+console.log(el)
