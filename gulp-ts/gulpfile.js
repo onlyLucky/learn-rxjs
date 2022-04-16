d70eb9ce6ac10f8c2c8adf808c025d69b761f474
feynman <2901954062@qq.com>
2022-04-16 21:26:00 +0800

    初始化gulpfiles配置 添加tsconfig

---
+++ gulp-ts/gulpfile.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: fg
+ * @Date: 2022-04-16 21:24:39
+ * @LastEditors: fg
+ * @LastEditTime: 2022-04-16 21:24:39
+ * @Description: gulp config
+ */
---
+++ gulp-ts/src/main.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: fg
+ * @Date: 2022-04-16 20:49:39
+ * @LastEditors: fg
+ * @LastEditTime: 2022-04-16 20:51:40
+ * @Description: gulp
+ */
+
+function hello (compiler: string) {
+  console.log(`hello from ${compiler}`)
+}
+
+hello('typescript')
