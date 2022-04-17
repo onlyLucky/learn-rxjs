b50105dc07a24282ee378f353732d653ba957ef7
feynman <2901954062@qq.com>
2022-04-17 15:04:38 +0800

    gulp ts 提交

--- gulp-ts/gulpfile.js
+++ gulp-ts/gulpfile.js
@@ -2,33 +2,16 @@
  * @Author: fg
  * @Date: 2022-04-16 21:24:39
  * @LastEditors: fg
- * @LastEditTime: 2022-04-16 23:19:55
+ * @LastEditTime: 2022-04-17 15:03:13
  * @Description: gulp config
  */
 
 const gulp = require('gulp')
-const browserify = require('browserify')
-const source = require('vinyl-source-stream')
-const tsify = require('tsify')
-const paths = {
-  pages: ['src/*.html']
-}
+const ts = require('gulp-typescript')
+const tsProject = ts.createProject('tsconfig.json')
 
-gulp.task('copy-html', function () {
-  return gulp.src(paths.pages)
-    .pipe(gulp.dest('dist'))
-})
-
-gulp.task('default', ['copy-html'], function () {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-  })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist'))
+gulp.task('default', () => {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist'))
 })
--- gulp-ts/src/main.ts
+++ gulp-ts/src/main.ts
@@ -2,15 +2,15 @@
  * @Author: fg
  * @Date: 2022-04-16 20:49:39
  * @LastEditors: fg
- * @LastEditTime: 2022-04-16 22:56:53
+ * @LastEditTime: 2022-04-17 15:03:19
  * @Description: gulp
  */
 
-import { sayHello } from './greet'
+// import { sayHello } from './greet'
 function hello (compiler: string) {
   console.log(`hello from ${compiler}`)
 }
 
 hello('typescript')
 
-console.log(sayHello('TypeScript'))
+// console.log(sayHello('TypeScript'))
