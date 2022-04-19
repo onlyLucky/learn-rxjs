b5db7b3dc5b25f281f0507d7166d485a8dd7cbdc
feynman <2901954062@qq.com>
2022-04-19 11:30:03 +0800

    interface 添加

--- ts-learn/interface.ts
+++ ts-learn/interface.ts
@@ -2,7 +2,7 @@
  * @Author: fg
  * @Date: 2022-04-18 18:19:20
  * @LastEditors: fg
- * @LastEditTime: 2022-04-18 19:01:24
+ * @LastEditTime: 2022-04-19 10:43:47
  * @Description: 接口
  */
 
@@ -59,4 +59,90 @@ interface ClockInterface{
   currentTime: Date;
 }
 
-class Clock implements 
+class Clock implements ClockInterface {
+  currentTime: Date;
+  setTime(d: Date){
+    this.currentTime = d;
+  }
+  constructor(h: number, m: number){}
+}
+
+// 类静态部分与实例部分
+interface ClockConstructor {
+  new (hour: number, minute: number);
+}
+
+class Clock implements ClockConstructor {
+  currentTime: Date;
+  constructor(h: number, m: number) { }
+}
+
+interface ClockInterface{
+  tick();
+}
+function createClock(ctor: ClockConstructor, hour: number, minute: number):ClockInterface{
+  return new ctor(hour, minute)
+}
+
+class DigitalClock implements ClockInterface{
+  constructor(h: number,m: number){}
+  tick() {
+    console.log('beep beep')
+  }
+}
+
+class AnalogClock implements ClockInterface{
+  constructor(h: number, m: number){}
+  tick() {
+    console.log('tick tick')
+  }
+}
+let digital = createClock(DigitalClock, 12, 17)
+let analog = createClock(AnalogClock, 7, 32)
+
+
+// 继承接口
+interface Shape {
+  color: string;
+}
+interface Square extends Shape{
+  sideLength: number;
+}
+
+let square = <Square>{}
+square.color = 'blue'
+square.sideLength = 10
+square.penWidth = 5
+
+// 混合类型
+interface Counter{
+  {start: number}: string;
+  interval: number;
+  reset(): void;
+}
+function getCounter(): Counter{
+  let counter = <Counter>function (start: number){}
+  counter.interval = 123
+  counter.reset = function(){}
+  return counter
+}
+let c = getCounter()
+c(10)
+c.reset()
+c.interval = 5
+
+// 接口继承类
+class Control{
+  private state: any;
+}
+interface SelectableControl extends Control {
+  select():void;
+}
+class Button extends Control implements SelectableControl{
+  select(): void {
+    
+  }
+}
+class TextBox extends Control {
+  
+}
