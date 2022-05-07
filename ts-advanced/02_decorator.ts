b91d824dc5f49c45cfa24b7b7ad96a1bfdf18670
feynman <2901954062@qq.com>
2022-05-07 13:16:00 +0800

    装饰器的执行顺序

--- ts-advanced/02_decorator.ts
+++ ts-advanced/02_decorator.ts
@@ -2,7 +2,7 @@
  * @Author: fg
  * @Date: 2022-05-03 23:51:44
  * @LastEditors: fg
- * @LastEditTime: 2022-05-04 17:22:23
+ * @LastEditTime: 2022-05-07 11:28:02
  * @Description: 装饰器
  */
 
@@ -18,3 +18,31 @@ function classFactory (): ClassDecorator {
 @a
 @b
 x
+
+
+function first() {
+  console.log("first(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("first(): called");
+  };
+}
+
+function second() {
+  console.log("second(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("second(): called");
+  };
+}
+
+class ExampleClass {
+  @first()
+  @second()
+  method() {}
+}
+// 会先执行first工厂函数、second工厂函数，
+// 再执行second工厂返回的装饰器、first工厂返回的装饰器函数
+// 因此，打印顺序为：
+// 'first(): factory evaluated'
+// 'second(): factory evaluated'
+// 'second(): called'
+// 'first(): called'
