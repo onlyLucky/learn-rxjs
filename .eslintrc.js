446681600c288ce8e95614ab5c58adc6719f712e
feynman <2901954062@qq.com>
2022-05-23 18:26:10 +0800

    chore: ts eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@
  * @Author: fg
  * @Date: 2022-04-14 10:21:44
  * @LastEditors: fg
- * @LastEditTime: 2022-04-14 18:56:47
+ * @LastEditTime: 2022-05-23 14:30:36
  * @Description: content
  */
 module.exports = {
   extends: [
     'standard'
   ],
+  parser: 'babel-eslint',
   // 如果你还需要检测 ts 文件则添加该项，并且保证根目录有 tsconfig.json 文件
   parserOptions: {
     project: './tsconfig.json'
