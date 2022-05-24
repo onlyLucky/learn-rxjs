b5f0c186be6b6aad3b082c6398b9bd359b687f97
feynman <2901954062@qq.com>
2022-05-24 11:10:51 +0800

    style: add docs

--- ts-challenges/00007-easy-readonly/template.ts
+++ ts-challenges/00007-easy-readonly/template.ts
@@ -1,4 +1,18 @@
+/* 
+返回一个对象
+
+遍历传入的对象（接口）   P in keyof T
+
+加上 readonly 关键字  readonly [P in keyof T]
+
+通过 key 获取传入对象（接口）的值，接口的值就是类型 [P in keyof T]: T[P]
+
+*/
+
+
 type MyReadonly<T> = {
   readonly [P in keyof T]: T[P]
 }
+
+
 
