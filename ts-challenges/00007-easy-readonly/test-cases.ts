0b36186c0427cb47e3df1cb11f1596bc0b76d592
feynman <2901954062@qq.com>
2022-05-24 10:22:42 +0800

    feat: ts challenges Readonly upload

---
+++ ts-challenges/00007-easy-readonly/test-cases.ts
@@ -0,0 +1,14 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Todo1 {
+  title: string,
+  description: string,
+  completed: boolean,
+  meta: {
+    author: string
+  }
+}
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
