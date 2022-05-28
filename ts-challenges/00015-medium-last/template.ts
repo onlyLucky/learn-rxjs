983e0d2a541ac57a7ccf28782ea3f69f7fed2902
feynman <2901954062@qq.com>
2022-05-28 17:07:01 +0800

    feat: ts-challenges 00015-medium-last 解法添加

--- ts-challenges/00015-medium-last/template.ts
+++ ts-challenges/00015-medium-last/template.ts
@@ -1,1 +1,7 @@
-type Last<T extends any[]> = any
+/* 
+Last
+
+*/
+
+type Last<T extends any[]> = T extends [...infer Others, infer L] ? L : never
+// type Last<T extends any[]> = [any, ...T][T['length']]
