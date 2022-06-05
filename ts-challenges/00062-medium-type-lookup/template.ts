debf9c458013aa6ef16cbd7055794f5d74c2f503
feynman <2901954062@qq.com>
2022-06-05 21:41:38 +0800

    feat: ts-challenges 62 answer upload

--- ts-challenges/00062-medium-type-lookup/template.ts
+++ ts-challenges/00062-medium-type-lookup/template.ts
@@ -1,1 +1,12 @@
-type LookUp<U, T> = any
+/*
+题目分析：
+想通过在联合Cat | Dog中搜索公共type字段来获取相应的类型
+
+解题思路：
+
+- 进行传参类型约束
+- 返回值进行类型约束
+
+*/
+
+type LookUp<U extends { type: string }, T extends string> = U extends { type: T } ? U : never
