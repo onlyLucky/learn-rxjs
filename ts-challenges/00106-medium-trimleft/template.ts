c41e73ddda0aa97568f44cf7879a5a8818b95dd0
feynman <2901954062@qq.com>
2022-06-05 22:36:42 +0800

    feat: ts-challenges TrimLeft upload

--- ts-challenges/00106-medium-trimleft/template.ts
+++ ts-challenges/00106-medium-trimleft/template.ts
@@ -1,1 +1,10 @@
-type TrimLeft<S extends string> = any
+/* 
+题目分析：也就是清空左侧的空格
+
+题目解析：
+- 首先确保入参类型为字符串类型
+- 空格符需要清除的有三种" "(空格符)、"\n"(换行符)、"\t"(制表符)
+
+*/
+
+type TrimLeft<S extends string> = S extends `${" " | "\n" | "\t"}${infer Rest}` ? TrimLeft<Rest> : S
