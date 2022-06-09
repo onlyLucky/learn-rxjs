be4c856f78568e2ebad59e9c5c0b1de2cb60f3e9
feynman <2901954062@qq.com>
2022-06-09 15:18:43 +0800

    feat: ts-challenges 00119-medium-replaceall answer upload

--- ts-challenges/00119-medium-replaceall/template.ts
+++ ts-challenges/00119-medium-replaceall/template.ts
@@ -1,1 +1,15 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+/* 
+
+解题思路
+
+- 可以利用刚才Replace实现进行递归处理
+- 根据测试案例得，替换后的字符串位置，不能再次进行匹配替换
+
+
+*/
+
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+? S
+: S extends `${infer L}${From}${infer R}`
+  ? `${L}${To}${ReplaceAll<R,From,To>}`
+  : S
