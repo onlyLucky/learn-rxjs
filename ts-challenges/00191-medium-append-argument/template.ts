5ffb79a3e14d42fbff4c25a99564925df5ca96f9
feynman <2901954062@qq.com>
2022-06-09 15:36:10 +0800

    feat: ts-challenges 00191-medium-append-argument answer upload

--- ts-challenges/00191-medium-append-argument/template.ts
+++ ts-challenges/00191-medium-append-argument/template.ts
@@ -1,1 +1,9 @@
-type AppendArgument<Fn, A> = any
+/*
+解题思路：
+- 将Fn函数的参数返回值解析出来
+- 判断当前能不能结构出来参数和返回值，不能的话就直接返回Fn，否则就将参数A塞到函数中
+*/
+
+type AppendArgument<Fn, A> = Fn extends (...args: infer Args) => infer R
+  ? (...args: [...Args, A]) => R
+  : Fn
