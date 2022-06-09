2f4e1a74e7ff9249b1796600b48af991c2b50cdd
feynman <2901954062@qq.com>
2022-06-09 13:50:02 +0800

    feat: ts-challenges 108 trim answer upload

--- ts-challenges/00108-medium-trim/template.ts
+++ ts-challenges/00108-medium-trim/template.ts
@@ -1,1 +1,6 @@
-type Trim<S extends string> = any
+/* 
+- 判断当前S传参左右是否存在空格
+- 否则继续递归直至返回
+*/
+
+type Trim<S extends string> = S extends `${' '| '\n' | '\t'}${infer P}`|`${infer P}${' '| '\n' | '\t'}`?Trim<P>:S
