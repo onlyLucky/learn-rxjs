42432b64b28dbd16cc7f2318da3107c5388fd93f
feynman <2901954062@qq.com>
2022-06-11 13:22:16 +0800

    feat: ts-challenges answer add

--- ts-challenges/00296-medium-permutation/template.ts
+++ ts-challenges/00296-medium-permutation/template.ts
@@ -1,1 +1,16 @@
-type Permutation<T> = any
+/*
+题目分析：
+- 单个元素时，全排列
+- 多个元素时，不考虑排列组合顺序
+- 为never时，结果为空数组
+
+代码实现
+
+
+*/
+
+type Permutation<T,U=T> = [T] extends [never]
+?[]
+: (T extends U
+  ? [T, ...Permutation<Exclude<U, T>>]
+  : [])
